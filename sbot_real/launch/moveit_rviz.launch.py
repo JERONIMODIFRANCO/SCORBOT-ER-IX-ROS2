57dd5b67c0739e956d016ea363271609de7ae74c
GastonGaspoz <glgaspoz@gmail.com>
2024-01-23 10:50:12 -0300

    H.I. de ejemplo

---
+++ sbot_real/launch/moveit_rviz.launch.py
@@ -0,0 +1,7 @@
+from moveit_configs_utils import MoveItConfigsBuilder
+from moveit_configs_utils.launches import generate_moveit_rviz_launch
+
+
+def generate_launch_description():
+    moveit_config = MoveItConfigsBuilder("scorbot", package_name="sbot_real").to_moveit_configs()
+    return generate_moveit_rviz_launch(moveit_config)
