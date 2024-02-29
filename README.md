4cfdb0301f571ca7c915e537975acd2b860ed9b2
JERONIMODIFRANCO <jero.jdf@gmail.com>
2024-02-29 19:22:52 -0300

    cambios minimos

---
+++ README.md
@@ -0,0 +1,6 @@
+<<<<<<< HEAD
+=======
+# SCORBOT-ER-IX-ROS2
+Ros2 packages for a control uptdate of a SCORBOT-ER-IX robot arm
+Packages developed for ROS2 Humble and Ignition Fortress 
+>>>>>>> 960ef0a (cambios minimos)
--- sbot_gazebo/launch/bridge.launch.py
+++ sbot_gazebo/launch/bridge.launch.py
@@ -40,22 +40,8 @@ def generate_launch_description():
             # Clock (IGN -> ROS2)
             '/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock',
             # Joint States (IGN -> ROS2)
-            # '/world/empty/model/scorbot/joint_state@sensor_msgs/msg/JointState[ignition.msgs.Model',
-        ],
-        # remappings=[
-        #     ('/world/empty/model/scorbot/joint_state', '/joint_states'), # "empty" para el modelo de mundo que estamos usando ya que se llama empty
-        # ],
-        output='screen'
-    )
-    returnList.append(bridgeDefaults)
-
-
-    bridgeJointCommands = Node(
-        package='ros_gz_bridge',
-        executable='parameter_bridge',
-        condition = IfCondition(LaunchConfiguration('bridge')),
-        arguments=[
-            # Joint Commands (ROS2 -> GZ)
+            '/world/default/model/scorbot/joint_state@sensor_msgs/msg/JointState[ignition.msgs.Model',
+             # Joint Commands (ROS2 -> GZ)
             '/model/scorbot/joint/base_joint/cmd_pos@std_msgs/msg/Float64]gz.msgs.Double',
             '/model/scorbot/joint/elbow/cmd_pos@std_msgs/msg/Float64]gz.msgs.Double',
             '/model/scorbot/joint/shoulder/cmd_pos@std_msgs/msg/Float64]gz.msgs.Double',
@@ -63,7 +49,6 @@ def generate_launch_description():
             '/model/scorbot/joint/roll/cmd_pos@std_msgs/msg/Float64]gz.msgs.Double',
             '/model/scorbot/joint/finger_R_joint/cmd_pos@std_msgs/msg/Float64]gz.msgs.Double',
             '/model/scorbot/joint/finger_L_joint/cmd_pos@std_msgs/msg/Float64]gz.msgs.Double',
-            '/world/default/model/scorbot/joint_state@sensor_msgs/msg/JointState[ignition.msgs.Model'
         ],
         remappings=[
             ('/model/scorbot/joint/base_joint/cmd_pos', '/commands/base_joint'),
@@ -73,10 +58,40 @@ def generate_launch_description():
             ('/model/scorbot/joint/roll/cmd_pos', '/commands/roll'),
             ('/model/scorbot/joint/finger_R_joint/cmd_pos', '/commands/finger_R_joint'),
             ('/model/scorbot/joint/finger_L_joint/cmd_pos', '/commands/finger_L_joint'),
-            ('/world/default/model/scorbot/joint_state', '/joint_states'),
+            ('/world/default/model/scorbot/joint_state', '/joint_states'), # "empty" para el modelo de mundo que estamos usando ya que se llama empty
         ],
         output='screen'
     )
-    returnList.append(bridgeJointCommands)
+    returnList.append(bridgeDefaults)
+
+
+    # bridgeJointCommands = Node(
+    #     package='ros_gz_bridge',
+    #     executable='parameter_bridge',
+    #     condition = IfCondition(LaunchConfiguration('bridge')),
+    #     arguments=[
+    #         # Joint Commands (ROS2 -> GZ)
+    #         '/model/scorbot/joint/base_joint/cmd_pos@std_msgs/msg/Float64]gz.msgs.Double',
+    #         '/model/scorbot/joint/elbow/cmd_pos@std_msgs/msg/Float64]gz.msgs.Double',
+    #         '/model/scorbot/joint/shoulder/cmd_pos@std_msgs/msg/Float64]gz.msgs.Double',
+    #         '/model/scorbot/joint/pitch/cmd_pos@std_msgs/msg/Float64]gz.msgs.Double',
+    #         '/model/scorbot/joint/roll/cmd_pos@std_msgs/msg/Float64]gz.msgs.Double',
+    #         '/model/scorbot/joint/finger_R_joint/cmd_pos@std_msgs/msg/Float64]gz.msgs.Double',
+    #         '/model/scorbot/joint/finger_L_joint/cmd_pos@std_msgs/msg/Float64]gz.msgs.Double',
+    #         # '/world/default/model/scorbot/joint_state@sensor_msgs/msg/JointState[ignition.msgs.Model'
+    #     ],
+    #     remappings=[
+    #         ('/model/scorbot/joint/base_joint/cmd_pos', '/commands/base_joint'),
+    #         ('/model/scorbot/joint/elbow/cmd_pos', '/commands/elbow'),
+    #         ('/model/scorbot/joint/shoulder/cmd_pos', '/commands/shoulder'),
+    #         ('/model/scorbot/joint/pitch/cmd_pos', '/commands/pitch'),
+    #         ('/model/scorbot/joint/roll/cmd_pos', '/commands/roll'),
+    #         ('/model/scorbot/joint/finger_R_joint/cmd_pos', '/commands/finger_R_joint'),
+    #         ('/model/scorbot/joint/finger_L_joint/cmd_pos', '/commands/finger_L_joint'),
+    #         # ('/world/default/model/scorbot/joint_state', '/joint_states'),
+    #     ],
+    #     output='screen'
+    # )
+    # returnList.append(bridgeJointCommands)
 
     return LaunchDescription(returnList)
